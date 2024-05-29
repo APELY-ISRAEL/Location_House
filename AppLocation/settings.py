ff99325613231e76c5f8640866f1bbf5be00df78
Israel APELY <israe@Israel.esagnde.local>
2024-05-29 18:07:08 +0000

    corrections

--- AppLocation/settings.py
+++ AppLocation/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+import MySQLdb
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +28,7 @@ SECRET_KEY = 'django-insecure-($l205mb*=g9jdxt3@h%t8d&j)5+d-ue!x111)h(9()bwaql7f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app', '.now.sh']
 
 
 
